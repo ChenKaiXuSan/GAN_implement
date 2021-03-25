c3375ebb52ae6163b1753229232fd841dbab3be9
chenkaixu@lab <chenkaixusan@gmail.com>
2021-03-25 13:38:28 +0900

    update

--- conditional_wgan/main.py
+++ conditional_wgan/main.py
@@ -44,7 +44,7 @@ parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of firs
 parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
 parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
 parser.add_argument("--img_size", type=int, default=32, help="size of each image dimension")
-parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("--channels", type=int, default=1, help="number of image channels")
 parser.add_argument("--n_critic", type=int, default=5, help="number of training steps for discriminator per iter")
 parser.add_argument("--clip_value", type=float, default=0.01, help="lower and upper clip value for disc. weights")
 parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
@@ -53,7 +53,7 @@ parser.add_argument("--dataset", type=str, choices=['mnist', 'fashion', 'cifar10
                     default='cifar10', help="dataset to use")
 parser.add_argument("--dataroot", type=str, default='../data/', help='path to dataset')
 
-opt = parser.parse_args([])
+opt = parser.parse_args()
 print(opt)
 # %%
 opt.img_shape = (opt.channels, opt.img_size, opt.img_size)
