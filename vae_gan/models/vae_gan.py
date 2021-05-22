d7a48706392d83896c60da0f1a3d01b3d6ef4de2
Xu <chenkaixusan@gmail.com>
2021-05-22 16:30:34 +0900

    update

--- vae_gan/models/vae_gan.py
+++ vae_gan/models/vae_gan.py
@@ -182,7 +182,7 @@ class Discriminator(nn.Module):
         )
 
         self.fc2 = nn.Sequential(
-            nn.Linear(512, channel_in), # todo
+            nn.Linear(512, 1), # todo
             nn.Sigmoid()
         )
     
